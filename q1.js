f93a7d7e2489a1c5943976759ed7c11cd0bd1ac6
anish0803 <singhanish124@gmail.com>
2022-06-28 22:49:10 +0530

    loop question pattern

---
+++ q1.js
@@ -0,0 +1,36 @@
+// q1. Print the pattern given below, using loops.
+
+// Input:
+// No input is required.
+
+// Output:
+
+// *****
+// *****
+// *****
+// *****
+
+
+//ans
+
+// let rows =4;
+// let cols= 5;
+
+// for(i=1;i<=rows;i++){
+//     let star='';
+//     for(j=1;j<=cols;j++){
+//     // console.log('*');
+//     star +='*';
+// }
+// console.log(star);
+// }
+
+
+for(i=1;i<=4;i++){
+    let star='';
+    for(j=1;j<=5;j++){
+    // console.log('*');
+    star +='*';
+}
+console.log(star);
+}
