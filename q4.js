5f4782e322e3b1c46dc55a0252967b39637f74be
anish0803 <singhanish124@gmail.com>
2022-06-28 22:54:55 +0530

    pattern q4

---
+++ q4.js
@@ -0,0 +1,23 @@
+// Question 4
+// Print the pattern given below, using loops.
+
+// Input:
+// No input is required.
+
+// Output:
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+//ans
+
+for(i=1;i<=5;i++){
+    let star='';
+    for(j=1;j<=i;j++){
+    // console.log('*');
+    star +=i;;
+}
+console.log(star);
+}
